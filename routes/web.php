72c87e02f9893439c0cf1b6d2ca619252e39be48
Onu Abah <abahonuh@gmail.com>
2021-08-03 16:27:10 +0100

    welcome message on / route

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return response()->json([
+        'status' => 'Greetings!',
+        'message' => 'Welcome to Onubrooks\' Meal Recommender API, have at it...'
+    ]);
 });
