b234f47eb2d0564fa320b69a80ab6bfd76143b5b
Onu Abah <abahonuh@gmail.com>
2021-08-03 15:03:57 +0100

    add routes and scribe

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,23 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
+Route::post('/user/creeate', function (Request $request) {
+    return $request->user();
+});
+Route::middleware('auth:api')->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+    Route::get('/user/allergies', function (Request $request) {
+        return $request->user();
+    });
+    Route::post('/user/allergies', function (Request $request) {
+        return $request->user();
+    });
+    Route::post('/user/recommendations', function (Request $request) {
+        return $request->user();
+    });
+});
+Route::post('/system/recommendations', function (Request $request) {
     return $request->user();
 });
